feb3f5bd8f537c72ca879c5ee21a63176f027175
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-06-28 18:09:24 +0530

    Update experience section

--- src/sections/experience/index.tsx
+++ src/sections/experience/index.tsx
@@ -5,20 +5,20 @@ export default function Experience() {
     return (
         <section id="Experience">
             <h1>Experience</h1>
-            <div className="experiences card">
+            <div className="experiences">
                 <article className="experience">
                     <h3>August, 2023</h3>
                     <h2><b>Software Engineer</b> at Cresen Solutions</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates saepe fugiat ipsum. Expedita commodi maxime facere blanditiis, error est iure?</p>
                 </article>
                 <article className="experience">
-                    <h3>August, 2023</h3>
-                    <h2><b>Software Engineer</b> at Cresen Solutions</h2>
+                    <h3>October, 2021</h3>
+                    <h2><b>Jr. Product Specialist</b> at Cognizant</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates saepe fugiat ipsum. Expedita commodi maxime facere blanditiis, error est iure?</p>
                 </article>
                 <article className="experience">
-                    <h3>August, 2023</h3>
-                    <h2><b>Software Engineer</b> at Cresen Solutions</h2>
+                    <h3>October, 2020</h3>
+                    <h2><b>Programmer Analyst</b> at Cognizant</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates saepe fugiat ipsum. Expedita commodi maxime facere blanditiis, error est iure?</p>
                 </article>
             </div>
