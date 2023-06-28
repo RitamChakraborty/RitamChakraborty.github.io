61d091297d486bff6d70861ba5dfc1d9d0da1330
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-06-28 20:19:32 +0530

    Update skill style

--- src/sections/skill/index.tsx
+++ src/sections/skill/index.tsx
@@ -8,56 +8,56 @@ export default function Skill() {
             <article>
                 <h2>Programming Languages</h2>
                 <div className="container">
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Java</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>TypeScript</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>JavaScript</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Dart</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Bash</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Python</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Kotlin</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>C</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>PHP</h3></div>
@@ -68,31 +68,31 @@ export default function Skill() {
             <article>
                 <h2>Frameworks</h2>
                 <div className="container">
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Spring Boot</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Angular</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Flutter</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>SolidJS</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>React</h3></div>
@@ -103,25 +103,25 @@ export default function Skill() {
             <article>
                 <h2>Databases</h2>
                 <div className="container">
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Postgres</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Mysql</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>MongoDB</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Radis</h3></div>
@@ -132,31 +132,31 @@ export default function Skill() {
             <article>
                 <h2>Tools</h2>
                 <div className="container">
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Git, GitHub</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>IntellJ IDEA</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Visual Studio Code</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Docker</h3></div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil omnis eligendi culpa obcaecati sunt iure!</p>
                     </div>
-                    <div className="card">
+                    <div className="box">
                         <div className="header">
                             <img src="https://skillicons.dev/icons?i=java" alt="java" />
                             <h3>Linux</h3></div>
