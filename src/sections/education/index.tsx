0e7e8fea4dc55839ed3b93918a30b24d903c0919
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-06-28 19:09:33 +0530

    Update education section

--- src/sections/education/index.tsx
+++ src/sections/education/index.tsx
@@ -3,29 +3,33 @@ import "./index.scss";
 
 export default function Eduction() {
     return (
-        <section className="Eduction">
+        <section id="Education">
             <h1>Eduction</h1>
             <div className="educations">
                 <article className="card">
                     <h2>Techno India University</h2>
                     <div className="content">
-                        <p>B. Tech in Computer Science Engineering</p>
-                        <p>2016 to 2020</p>
-                        <p>8.46 CGPA</p>
+                        <p><b>B. Tech in Computer Science Engineering</b></p>
+                        <div className="container">
+                            <p>2016 to 2020</p>
+                            <p><i>8.46 CGPA</i></p>
+                        </div>
                     </div>
                 </article>
                 <article className="card">
                     <h2>Midnapore Collegiate School</h2>
                     <div className="content">
-                        <p>5th to 12th Standard</p>
-                        <p>2008 to 2016</p>
-                        <p>85% in 10th and 72.8% in 12th</p>
+                        <p><b>5th to 12th Standard</b></p>
+                        <div className="container">
+                            <p>2008 to 2016</p>
+                            <p><i>85% in 10th and 72.8% in 12th</i></p>
+                        </div>
                     </div>
                 </article>
                 <article className="card">
                     <h2>Saraswati Sishu Mandir</h2>
                     <div className="content">
-                        <p>Nursery to 4th Standard</p>
+                        <p><b>Nursery to 4th Standard</b></p>
                         <p>2002 to 2008</p>
                     </div>
                 </article>
