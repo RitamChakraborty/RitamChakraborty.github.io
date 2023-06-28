0dae28e994c9504b58a624c3265da14f70b8141e
Shuktika Mahanty <shuktika.mahanty@gmail.com>
2023-06-28 13:11:36 +0530

    Setup new project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './views/Header/Header'
+import App from '.'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
+    <App />
   </React.StrictMode>
 )
