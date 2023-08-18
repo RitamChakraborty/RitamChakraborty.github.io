d0e34b6cea7f9fabb678640e2d0b95e3e32141bd
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-08-18 21:20:32 +0530

    Update build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'static',
+    vite: {
+      build: {
+        rollupOptions: {
+          output: {
+            assetFileNames: 'assets/index.css',
+          }
+        }
+      }
+    }
+  });
